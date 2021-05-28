06e407f0238c983ccbcc5e8391958700eada4753
Anders Jensen-Urstad <anders.jensen-urstad@kb.se>
2021-05-28 17:51:56 +0200

    Use template file to make the notice translatable and move stuff out of JS

--- cookieNotice/js/cookieNotice.js
+++ cookieNotice/js/cookieNotice.js
@@ -1,19 +1,10 @@
-let cookieInfoUrl = 'https://publicera.kb.se/kakor';
-let cookieDurationDays = 365;
-
 document.addEventListener('DOMContentLoaded', function() {
   if (!document.cookie.split(';').some((item) => item.trim().startsWith('cookieNotice='))) {
-      const confirmationHtml = `
-        <div id="cookie-notice" class="alert alert-primary" role="alert">
-          <div class="container">
-            Genom att använda Publicera godkänner du de kakor (cookies) som finns på webbplatsen. <a href="${cookieInfoUrl}">Läs mer om hur vi använder kakor</a>.
-            <button id="cookie-notice-confirmation" type="button" class="btn btn-secondary btn-light btn-sm">OK, jag förstår</button>
-          </div>
-        </div>
-      `;
-      document.body.insertAdjacentHTML('afterbegin', confirmationHtml);
+      let cookieNotice = document.getElementById('cookie-notice');
+      document.body.insertAdjacentElement('afterbegin', cookieNotice);
+
       document.getElementById('cookie-notice-confirmation').addEventListener('click', function() {
-        document.cookie = `cookieNotice=1; max-age=${60*60*24*cookieDurationDays}`;
+        document.cookie = `cookieNotice=1; max-age=${60*60*24*365}; SameSite=Strict`;
         document.getElementById('cookie-notice').classList.add('cookie-notice-hidden');
       });
   }
---
+++ cookieNotice/templates/cookieNotice.tpl
@@ -0,0 +1,8 @@
+<div id="cookie-notice" class="alert alert-primary" role="alert">
+  <div class="container">
+    {translate key='plugins.generic.cookieNotice.infoText'}
+    <button id="cookie-notice-confirmation" type="button" class="btn btn-secondary btn-light btn-sm">
+      {translate key='plugins.generic.cookieNotice.confirmationButtonText'}
+    </button>
+  </div>
+</div>
