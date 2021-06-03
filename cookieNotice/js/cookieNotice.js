2b0677b3bf1c57a1162c49bf5e180f28667e40b4
Anders Jensen-Urstad <anders.jensen-urstad@kb.se>
2021-06-03 11:40:49 +0200

    Add basic styles to ensure notice is fixed even on journal pages; set path=/ in cookie

--- cookieNotice/js/cookieNotice.js
+++ cookieNotice/js/cookieNotice.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   if (!document.cookie.split(';').some((item) => item.trim().startsWith('cookieNotice='))) {
       document.getElementById('cookie-notice-confirmation').addEventListener('click', function() {
-        document.cookie = `cookieNotice=1; max-age=${60*60*24*365}; SameSite=Strict`;
+        document.cookie = `cookieNotice=1; max-age=${60*60*24*365}; SameSite=Strict; path=/`;
         document.getElementById('cookie-notice').classList.add('cookie-notice-hidden');
       });
   }
